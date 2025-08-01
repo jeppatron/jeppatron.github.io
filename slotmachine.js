7cdfec48a877e92bd675c0ded4fb9455f9cbd704
jeppatron <jefferson.adj10@gmail.com>
2025-08-01 10:48:17 +0700

    Update slotmachine.js

--- slotmachine.js
+++ slotmachine.js
@@ -1,17 +1,33 @@
+var rolls = 0;
+var rollGoods = 0;
+var jackpots = 0;
+
 function rollFunction() {
+    rollAmount.textContent = rolls += 1;
     slotOne.textContent = Math.floor(Math.random() * 10);
     slotTwo.textContent = Math.floor(Math.random() * 10);
     slotThree.textContent = Math.floor(Math.random() * 10);
     outputText.classList.remove("hidden");
-    if (slotOne.textContent == slotTwo.textContent && slotTwo.textContent == slotThree.textContent && slot_One.textContent > 0) {
+    if (slotOne.textContent == slotTwo.textContent && slotTwo.textContent == slotThree.textContent && slotOne.textContent > 0) {
         if (slotOne.textContent == 7) {
+            slots.classList.remove("rollGood");
+            slots.classList.remove("rollFail");
+            slots.classList.add("jackpot");
             outputText.textContent = "JACKPOT!!";
+            jackpotAmount.textContent = jackpots += 1;
         }
         else {
+            slots.classList.remove("rollFail");
+            slots.classList.remove("jackpot");
+            slots.classList.add("rollGood");
             outputText.textContent = "Oh, nice!";
+            goodRollAmount.textContent = rollGoods += 1;
         }
     }
     else {
+        slots.classList.remove("rollGood");
+        slots.classList.remove("jackpot");
+        slots.classList.add("rollFail");
         outputText.textContent = "Oh, that's sad.";
     }
 }
